b96920b291861f9b1ff83e00a8a83f4548913b94
Zaki Rizky <ybreinstsu@gmail.com>
2023-08-09 21:54:17 +0700

    pjt(FE): added misc pages for w3school ()

---
+++ HTML Project/w3schools/script/main.js
@@ -0,0 +1,14 @@
+var body = document.getElementById('body');
+var j = 1;
+function changeBackground() {
+    j = j > 2 ? 0 : j;
+    console.log(j);
+    if (j === 1) {
+        body.style.backgroundImage = 'url("./assets/shirokurokami.jpg")';
+    } else if (j === 2) {
+        body.style.backgroundImage = 'url("./assets/Zeta.png")';
+    } else {
+        body.style.backgroundImage = 'url("./assets/hoshimachi.jpg")';
+    }
+    j++;
+};
